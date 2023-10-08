05be3baf7158cf53fb7e3152e71bda325e7e657a
Barbara-Cz <56848157+Barbara-Cz@users.noreply.github.com>
2023-10-08 12:55:49 +0200

    Update Shop2ProductPage.java

--- Excercise1/src/test/java/shop2/Shop2ProductPage.java
+++ Excercise1/src/test/java/shop2/Shop2ProductPage.java
@@ -7,8 +7,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
-//Wybrać rozmiar M
-//Wybrać 5 sztuk, dodać do koszyka, proceed to check out- pop up window
+
 
 public class Shop2ProductPage {
     private final WebDriver driver;
